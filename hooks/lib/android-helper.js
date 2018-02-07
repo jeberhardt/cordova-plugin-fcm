ea8b7697bca97c33b6966898c5132f53d1e0fdd5
James Eberhardt <james@eberhardt.ca>
2018-02-06 23:13:50 -0500

    Update to remove Gradle modifications upon uninstall of plugin.

--- hooks/lib/android-helper.js
+++ hooks/lib/android-helper.js
@@ -11,10 +11,31 @@ module.exports = {
 
         buildGradle +=  [
             "",
+            "// Google Play Services Plugin - Start ",
+            "buildscript {",
+            "    repositories {",
+            "        maven { url 'https://maven.google.com' }"
+            "    }",
+            "    dependencies {",
+            "        classpath 'com.google.gms:google-services:3.1.0'"
+            "    }",
+            "}",
+            "",
             "apply plugin: 'com.google.gms.google-services'",
+            "// Google Play Services Plugin - End ",
         ].join("\n");
 
         utilities.writeBuildGradle(buildGradle);
     }
+},
+
+removeGooglePlayServicesFromGradle: function() {
+
+    var buildGradle = utilities.readBuildGradle();
+
+    buildGradle = buildGradle.replace(/\n\/\/ Google Play Services Plugin - Start[\s\S]*\/\/ Google Play Services Plugin - End/, "");
+
+    utilities.writeBuildGradle(buildGradle);
+}
 
 };
